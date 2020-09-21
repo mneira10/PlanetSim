bfdfc143fb0a090a63ca729cf823e471a6a87af1
Mauricio Neira <m.neira10@uniandes.edu.co>
2020-09-21 01:26:40 -0500

    basic 2 bodies

--- body.js
+++ body.js
@@ -1,39 +1,123 @@
-function Body(m, r, x, y, vx, vy) {
-    this.m = m;
-    this.r = r;
-    this.pos = createVector(x,y);
-    this.v = createVector(vx,vy);
-    this.deltat = 0.1; 
+function drawArrow(base, vec, myColor) {
+    push();
+    stroke(myColor);
+    strokeWeight(1);
+    fill(myColor);
+    translate(base.x, base.y);
+    line(0, 0, vec.x, vec.y);
+    rotate(vec.heading());
+    let arrowSize = 7;
+    translate(vec.mag() - arrowSize, 0);
+    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+    pop();
+}
 
-    this.display = () => {
+
+class Body {
+    constructor(id, m, r, x, y, vx, vy) {
+        // console.log('x,y', x, y,);
+        this.id = id;
+        this.m = m;
+        this.radius = r;
+        this.v = createVector(vx, vy);
+        this.vhalf = null;
+        this.r = createVector(x, y);
+        // console.log('r vector:', this.r);
+        this.deltat = 0.1;
+        this.G = 1000;
+        // console.log(this);
+    }
+
+    logVector(vector) {
+        let logObj = { 'x': vector == null ? null : vector.x, 'y': vector == null ? null : vector.y }
+        return JSON.parse(JSON.stringify(logObj))
+    }
+    log() {
+        const objToLog = {
+            'id': this.id, 'm': this.m, 'radius': this.radius,
+            'v': this.logVector(this.v), 'vhalf': this.logVector(this.vhalf),
+            'r': this.logVector(this.r), 'rnext': this.logVector(this.rnext), 'deltat': this.deltat, 'G': this.G
+        }
+
+        console.log(JSON.parse(JSON.stringify(objToLog)))
+    }
+
+
+    display() {
         stroke(255);
         fill(255, 100);
-        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+        ellipse(this.r.x, this.r.y, this.radius * 2, this.radius * 2);
+
+        this.v.mag() !== 0 && drawArrow(this.r, this.v, 255);
     }
 
-    this.distance = (body2) => {
-        return Math.sqrt((this.pos.x - body2.pos.x)(this.pos.x - body2.pos.x) + (this.pos.y - body2.pos.y) * (this.pos.y - body2.pos.y));
+    distance(body2) {
+        return Math.sqrt((this.r.x - body2.pos.x)(this.r.x - body2.pos.x) + (this.r.y - body2.pos.y) * (this.r.y - body2.pos.y));
     }
 
-    this.overlaps = (body2) => {
+    overlaps(body2) {
+        console.log('overlaps')
         const distance = this.distance(body2);
-        return (this.r + body2.r) >= distance;
+        return (this.radius + body2.r) >= distance;
     }
 
-    this.getAcceleration = ()=> {
+    force(body2, presentPosition) {
+        // console.log(body2);
+        if (this.id == body2.id) { return createVector(0, 0) }
+
+        // console.log('force for body', this.id, 'and', body2.id)
+        // this.log()
+        const r21 = body2.r.copy().sub(presentPosition ? this.r : this.rnext);
+        // console.log('r21', r21);
+        // this.log()
+        // body2.log()
+        let f21 = r21.copy().normalize();
+        // console.log('r21 vector:', r21, r21.magSq());
+        const forceMag = this.m * body2.m * this.G / r21.magSq();
+
+        const returnForceVect = f21.mult(forceMag);
+        // console.log('force:', returnForceVect)
+        // console.log('forceMag', forceMag)
+        return returnForceVect;
+    }
+
+    getAcceleration(bodies, presentPosition = true) {
+        // console.log('accel for id:', this.id)
+        // console.log('accel body')
+        // this.log();
+        let a = createVector(0, 0);
+        for (let body of bodies) {
+            const df = this.force(body, presentPosition);
+            a = a.add(df.div(this.m));
+        }
+        // console.log('acceleration:', a)
+        return a;
+    }
+    updateR(bodies) {
+        // console.log('Update r with id:', this.id);
+
+        const a = this.getAcceleration(bodies);
+
+        this.vhalf = this.v.copy().add(a.mult(this.deltat / 2));
+        this.rnext = this.r.copy().add(this.vhalf.copy().mult(this.deltat));
 
     }
 
-    this.update = (forcex, forcey, deltat)=>{
-        const ax = forcex/this.m;
-        const ay = forcey/this.m;
+    updateVelocity(bodies) {
+        // console.log('Update velocity for id', this.id);
+        const a = this.getAcceleration(bodies, false);
+        this.v = this.vhalf.copy().add(a.mult(this.deltat / 2));
+        this.r = this.rnext.copy();
+    }
+}
 
-        const vhalfx = this.vx + ax*this.deltat/2;
-        const vhalfy = this.vy + ay*this.deltat/2;
 
-        this.x = this.x + vhalfx*this.deltat;
-        this.y = this.y + vhalfy*this.deltat;
+class Coso {
+    constructor(a) {
+        this.a = a
+    }
 
-        this.vx = vhalfx + ax
+    updateA(items) {
+        this.a = items.length;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,13 +1,70 @@
+var bodiesFuture = [];
+var bodiesPresent = [];
+let cont = 0;
+
+function logBodies(bodies){
+    for(let body of bodies){
+        body.log()
+    }
+}
+
+function logAllBodies(){
+    console.log('bodiesFuture');
+    logBodies(bodiesFuture)
+    console.log('bodiesPResent');
+    logBodies(bodiesPresent)
+}
+
+
 function setup() {
-    createCanvas(400, 400);
-    b1 = new Body(1,33,20,40,20,20);
-    b2 = new Body(1,40,40,40,20,20);
+    console.log(JSON.stringify(bodiesFuture))
+    createCanvas(windowWidth, windowHeight);
+    console.log(windowHeight, windowWidth)
+    let b2 = new Body(1, 1000, 10, windowWidth / 3.0, windowHeight / 2.0, 0, -55);
+    // console.log('b2', b2);
+    let b1 = new Body(2, 1000, 10, windowWidth / 2.0, windowHeight / 2.0, 0, 55);
+    let b3 = new Body(2, 1000, 10, windowWidth / 4.0, windowHeight / 4.0, 0, 0);
+    // console.log('b1', b1);
+    bodiesFuture.push(b2);
+    bodiesFuture.push(b1);
+    // bodiesFuture.push(b3);
+
+    bodiesPresent = [...bodiesFuture];
+    console.log('in setup')
+    logAllBodies();
+    
+
+    // noLoop();
 }
 
 function draw() {
     background(0);
-    background(0);
+    // console.log(cont)
+    // logAllBodies();
+    for (let body of bodiesPresent) {
+        body.display();
+    }
 
-    b1.display();
-    b2.display();
+    for (let body of bodiesFuture) {
+        // console.log('originalBodies:', bodiesPresent);
+        // body.r = 2
+        body.updateR(bodiesPresent);
+    }
+
+    for (let body of bodiesFuture) {
+        body.updateVelocity(bodiesFuture);
+    }
+
+    bodiesPresent = [...bodiesFuture];
+
+    // if(cont<3){
+    //     loop();
+    // }
+    // console.log('before exit')
+    // logAllBodies()
+    cont = cont + 1;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
